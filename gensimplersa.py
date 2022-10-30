a84fd24735cf52be85b731523709db7f4b78134c
The IT Guy <33011744+theitguynumber1@users.noreply.github.com>
2022-10-30 16:26:42 +1100

    Create gensimplersa.py

---
+++ gensimplersa.py
@@ -0,0 +1,3 @@
+# This script generates 2 simple rsa keys using small prime numbers just to exhibit how the algorithm works! 
+# Obviously, the keys generated this way are in now way or form secure and should not be used. 
+
